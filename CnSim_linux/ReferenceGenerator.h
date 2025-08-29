8b03f3c9fb235b96d07f3958e32099a4a96f4d70
hc.pyon <hc.pyon@uos.ac.kr>
2025-08-29 16:52:00 +0900

    reference generator functionalize, before adding alpha calculator in class.

--- CnSim_linux/ReferenceGenerator.h
+++ CnSim_linux/ReferenceGenerator.h
@@ -6,32 +6,32 @@
 
 
 class ReferenceGenerator {
-public:
-    // 생성자
-    ReferenceGenerator(int rotate_direction, std::array<double, 6> alpha_coeffs_, double final_operating_time);
-    
-    // 5차 다항식 궤적 계수를 계산하는 함수
-    // Input:
-    //   t_s: 시작 시간
-    //   t_f: 끝 시간
-    //   theta_s: 시작 위치
-    //   theta_dot_s: 시작 속도
-    //   theta_ddot_s: 시작 가속도
-    //   theta_f: 끝 위치
-    //   theta_dot_f: 끝 속도
-    //   theta_ddot_f: 끝 가속도
 
-    // 특정 시간 t에 대한 목표 위치를 계산
-    double get_position(double current_time);
-    
-    // 특정 시간 t에 대한 목표 속도를 계산
-    double get_velocity(double current_time);
-    
-    // 특정 시간 t에 대한 목표 가속도를 계산
-    double get_acceleration(double current_time);
+    public:
+        std::array<double, 6> alpha_coeffs; // 다항식 계수들
+        std::array<double, 3> start_state; // 시작 상태 [위치, 속도, 가속도]
+        std::array<double, 3> final_state; // 최종 상태 [위치, 속도, 가속도]
+        double time_ref_start; // 궤적 시작 시간
+        double time_ref_fin; // 궤적 종료 시간
+        
+    public:
+        // 생성자
+        ReferenceGenerator();
+        std::array<double, 6> computeAlphaCoeffs(double time_ref_start_,
+                                                double time_ref_fin_,
+                                                std::array<double, 3> start_state_,
+                                                std::array<double, 3> final_state_
+                                                std::array<double, 6> alpha_coeffs_);
 
-private:
-    double alpha_coeffs[6];
-    double time_ref_start, time_ref_fin;
-    double current_joint1_position;
+        // 특정 시간 t에 대한 목표 위치를 계산
+        double get_position(double current_time_,
+                            double current_joint_position_);
+        
+        // 특정 시간 t에 대한 목표 속도를 계산
+        double get_velocity(double current_time_,
+                            double current_joint_velocity_);
+        
+        // 특정 시간 t에 대한 목표 가속도를 계산
+        double get_acceleration(double current_time_,
+                                double current_joint_acceleration_);
 };
